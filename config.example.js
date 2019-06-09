cd0cd36e45cefde7c4211459a98ba625816dec0b
SD SkyKlouD <sdsk@sdsk.one>
2019-06-10 00:06:14 +0900

    Add config example

---
+++ config.example.js
@@ -0,0 +1,8 @@
+module.exports = {
+    jsonFix: "window.YTD.tweet.part0 = ",   // Text to be removed to parse tweet.js as JSON correctly
+
+    consumerKey: "",                        // Twitter application consumer key
+    consumerSecret: "",                     // Twitter application consumer secret
+    accessToken: "",                        // Twitter application access token
+    accessTokenSecret: ""                   // Twitter application access token secret
+}
